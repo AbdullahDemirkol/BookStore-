fa5cbbbfc814408e4924752cd36d64a1cadc1c4d
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-09-18 22:52:36 +0300

    Auto increment added for id colon

--- WebAPI/Book.cs
+++ WebAPI/Book.cs
@@ -1,7 +1,10 @@
-﻿namespace WebAPI
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI
 {
     public class Book
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Title { get; set; }
         public string GenreId { get; set; }
--- WebAPI/DataAccess/DataGenerator.cs
+++ WebAPI/DataAccess/DataGenerator.cs
@@ -22,7 +22,7 @@ namespace WebAPI.DataAccess
             var books= new List<Book>(){
                 new Book()
                 {
-                    Id = 1,
+                    //Id = 1,
                     Title = "Lean Startup",
                     GenreId = "1",
                     PageCount = 200,
@@ -30,7 +30,7 @@ namespace WebAPI.DataAccess
                 },
                 new Book()
                 {
-                    Id = 2,
+                    //Id = 2,
                     Title = "Herland",
                     GenreId = "2",
                     PageCount = 250,
@@ -38,7 +38,7 @@ namespace WebAPI.DataAccess
                 },
                 new Book()
                 {
-                    Id = 3,
+                    //Id = 3,
                     Title = "Dune",
                     GenreId = "2",
                     PageCount = 540,
